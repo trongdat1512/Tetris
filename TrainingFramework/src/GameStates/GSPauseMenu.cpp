b6ff0eb260b8839c51ebfeb47e4ba3f9c55ff98a
trongdat1512 <59142233+trongdat1512@users.noreply.github.com>
2020-03-11 20:45:30 +0700

    First commit

---
+++ TrainingFramework/src/GameStates/GSPauseMenu.cpp
@@ -0,0 +1,98 @@
+#include "GSPauseMenu.h"
+
+extern int screenWidth; //need get on Graphic engine
+extern int screenHeight; //need get on Graphic engine
+
+GSPauseMenu::GSPauseMenu()
+{
+
+}
+
+
+GSPauseMenu::~GSPauseMenu()
+{
+}
+
+
+
+void GSPauseMenu::Init()
+{
+	auto model = ResourceManagers::GetInstance()->GetModel("Sprite2D");
+	auto texture = ResourceManagers::GetInstance()->GetTexture("bg_play");
+
+	//BackGround
+	auto shader = ResourceManagers::GetInstance()->GetShader("TextureShader");
+	m_BackGround = std::make_shared<Sprite2D>(model, shader, texture);
+	m_BackGround->Set2DPosition(screenWidth / 2, screenHeight / 2);
+	m_BackGround->SetSize(screenWidth, screenHeight);
+
+	//resume button
+	texture = ResourceManagers::GetInstance()->GetTexture("resume_btn");
+	std::shared_ptr<GameButton> button = std::make_shared<GameButton>(model, shader, texture);
+	button->Set2DPosition(screenWidth / 2, 200);
+	button->SetSize(200, 50);
+	button->SetOnClick([]() {
+		GameStateMachine::GetInstance()->ChangeState(StateTypes::STATE_Play);
+	});
+	m_listButton.push_back(button);
+	
+	//text game title
+	shader = ResourceManagers::GetInstance()->GetShader("TextShader");
+	std::shared_ptr<Font> font = ResourceManagers::GetInstance()->GetFont("telelower");
+	m_Text_gameName = std::make_shared< Text>(shader, font, "PAUSE", TEXT_COLOR::GREEN, 2.0);
+	m_Text_gameName->Set2DPosition(Vector2(screenWidth / 2 - 80, 120));
+}
+
+void GSPauseMenu::Exit()
+{
+}
+
+
+void GSPauseMenu::Pause()
+{
+
+}
+
+void GSPauseMenu::Resume()
+{
+
+}
+
+
+void GSPauseMenu::HandleEvents()
+{
+
+}
+
+void GSPauseMenu::HandleKeyEvents(int key, bool bIsPressed)
+{
+
+}
+
+void GSPauseMenu::HandleTouchEvents(int x, int y, bool bIsPressed)
+{
+	for (auto it : m_listButton)
+	{
+		(it)->HandleTouchEvents(x, y, bIsPressed);
+		if ((it)->IsHandle()) break;
+	}
+}
+
+void GSPauseMenu::Update(float deltaTime)
+{
+	m_BackGround->Update(deltaTime);
+	for (auto it : m_listButton)
+	{
+		it->Update(deltaTime);
+	}
+}
+
+void GSPauseMenu::Draw()
+{
+	m_BackGround->Draw();
+	for (auto it : m_listButton)
+	{
+		it->Draw();
+	}
+	m_Text_gameName->Draw();
+}
